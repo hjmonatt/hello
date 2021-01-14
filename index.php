9a2e0744255aecd1bbcd6358b3b9b1a924e4c118
Heather Monatt <hmonatt@mail.greenriver.edu>
2021-01-14 15:33:51 -0500

    Added home page view

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+//This is my CONTROLLER
+
 //Turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
@@ -10,6 +12,12 @@ require_once('vendor/autoload.php');
 //Create an instance of the Base class
 $f3 = Base::instance();
 $f3->set('DEBUG', 3);
+
+//Define a default route (home page)
+$f3->route('GET /', function() {
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
 
 //Run fat free
 $f3->run();
